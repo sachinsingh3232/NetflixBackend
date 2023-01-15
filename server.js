27c86c25e9e23c1ba88aabeb90b2e1739e48617c
SACHIN SINGH <200104049@hbtu.ac.in>
2023-01-15 23:20:38 +0530

    changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv')
 dotenv.config({ path: "./Config/.env" })
 const connectDB = require('./Config/database')
 connectDB();
-
-app.listen(5000, () => {
-    console.log("server running on port 5000")
+const PORT =process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`)
 })
